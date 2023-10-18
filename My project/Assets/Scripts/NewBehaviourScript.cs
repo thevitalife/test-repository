d2d2ee9854b730195356dc6b8a6950a3d5d78d28
Vitalii Kryvyi <vitalife@raccoons.games>
2023-10-18 09:13:02 +0300

    Added integer commit

--- My project/Assets/Scripts/NewBehaviourScript.cs
+++ My project/Assets/Scripts/NewBehaviourScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField]
+    private int integer;
     // Start is called before the first frame update
     void Start()
     {
